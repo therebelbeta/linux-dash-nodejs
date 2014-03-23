c08a1b074f4adbe4a336463474e3653eca546285
Trent Oswald <trentoswald@therebelrobot.com>
2014-03-23 16:42:07 -0600

    updating apis

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(express.urlencoded());
 app.use('/', express.static(__dirname + '/public/'));
 
 /*TODO: refactor api endpoint objects*/
-app.post('/api/sample', api.samplefunc); // local account check
+app.get('/api/df', api.df);
+app.get('/api/hostname', api.hostname);
+app.get('/api/uptime', api.uptime);
 
 //Initiate Server
 app.listen(8080, function () {
